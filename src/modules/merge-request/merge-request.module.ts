7e1d9655efff1bfa92e8841188fe98325c9705dd
c3b5aw <dev@c3b5aw.dev>
2022-06-17 15:18:38 +0200

    feat: add approval merge request message

--- src/modules/merge-request/merge-request.module.ts
+++ src/modules/merge-request/merge-request.module.ts
@@ -3,6 +3,7 @@ import { DatabaseModule } from 'app/modules/database/database.module';
 import { DiscordModule } from 'app/modules/discord/discord.module';
 import { GitlabModule } from 'app/modules/gitlab-client.module';
 import { MergeRequestController } from 'app/modules/merge-request/merge-request.controller';
+import { MergeRequestApprovedService } from 'app/modules/merge-request/services/merge-request-approved.service';
 import { MergeRequestClosedService } from 'app/modules/merge-request/services/merge-request-closed.service';
 import { MergeRequestOpenedService } from 'app/modules/merge-request/services/merge-request-opened.service';
 import { MergeRequestUpdatedService } from 'app/modules/merge-request/services/merge-request-updated.service';
@@ -15,6 +16,7 @@ import { MergeRequestValidationService } from 'app/modules/merge-request/service
     MergeRequestOpenedService,
     MergeRequestUpdatedService,
     MergeRequestClosedService,
+    MergeRequestApprovedService,
   ],
   controllers: [MergeRequestController],
 })
---
+++ src/modules/merge-request/services/merge-request-approved.service.ts
@@ -0,0 +1,97 @@
+import { UserSchema } from '@gitbeaker/core/dist/types/types';
+import { Injectable } from '@nestjs/common';
+import { GitlabClient } from 'app/libs/gitlab/client';
+import { MrApprovedWebhookPayload } from 'app/libs/gitlab/dtos/mr-approved.interface';
+import { logger } from 'app/libs/logger';
+import { DatabaseClient } from 'app/modules/database/database.service';
+import { DiscordService } from 'app/modules/discord/discord.service';
+import { MessageEmbed } from 'discord.js';
+
+@Injectable()
+export class MergeRequestApprovedService {
+  constructor(
+    private readonly db: DatabaseClient,
+    private readonly discord: DiscordService,
+    private readonly gitlab: GitlabClient,
+  ) {}
+
+  async notifyAssigneesForApproval(
+    gitlabUsernames: string[],
+    author: Omit<UserSchema, 'created_at'>,
+    projectName: string,
+    mrTitle: string,
+    mrUrl: string,
+  ) {
+    author.username = 'Mr. Bot';
+    for (const gitlabUsername of gitlabUsernames) {
+      logger.log(
+        `Notifying assignee ${gitlabUsername} for '${projectName}/${mrTitle}' approval`,
+      );
+
+      const trackers = await this.db.tracker.findMany({
+        where: {
+          gitlabUsername,
+        },
+        include: { user: true },
+      });
+
+      const idsToNotify = trackers
+        .filter(
+          (tracker) =>
+            !(
+              tracker.gitlabUsername === author.username &&
+              tracker.user.gitlabUsername === author.username
+            ),
+        )
+        .map((tracker) => tracker.user.discordId);
+
+      for (const id of idsToNotify) {
+        logger.log(`Notifying ${id}`);
+        const user = this.discord.users.cache.get(id);
+        if (user) {
+          logger.log(`Notifying user ${id} for mr ${mrTitle}`);
+          user.send({
+            embeds: [
+              new MessageEmbed()
+                .setTitle(
+                  `Merge request ${mrTitle} approved by ${author.username}`,
+                )
+                .setColor('#409bd7')
+                .setDescription(
+                  `A merge request your are assigned on **${projectName}** has been approved.\n\n**[${mrTitle}](${mrUrl})**`,
+                )
+                .setThumbnail(
+                  'https://about.gitlab.com/images/press/logo/png/gitlab-icon-rgb.png',
+                )
+                .setTimestamp(),
+            ],
+          });
+        } else {
+          logger.warn(`User ${id} not in cache`);
+        }
+      }
+    }
+  }
+
+  async handlerMergeRequestApproved(payload: MrApprovedWebhookPayload) {
+    const { assignees, object_attributes, project } = payload;
+
+    const author = await this.gitlab.Users.show(object_attributes.author_id);
+    const isDraft = object_attributes.title.includes('Draft:');
+    if (isDraft) {
+      logger.warn(
+        `Skipping merge request ${object_attributes.id} because it is drafted`,
+      );
+    } else {
+      if (assignees) {
+        this.notifyAssigneesForApproval(
+          assignees.map((a) => a.username),
+          author,
+          project.name,
+          object_attributes.title,
+          object_attributes.url,
+        );
+      }
+    }
+  }
+}
